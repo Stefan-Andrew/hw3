5aa20a8ebb602816edc43e224810631486a4ace1
Stefan cao <stefanc1@uci.edu>
2017-03-04 15:02:33 -0800

    implemented stride.cu, seems to work

--- cuda.sh
+++ cuda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -N REDUCE
+#$ -N STRIDE
 #$ -q gpu
 #$ -l gpu=1
 #$ -pe gpu-node-cores 6
@@ -42,7 +42,7 @@ echo ""
 echo "=== Running 5 trials of naive ... ==="
 for trial in 1 2 3 4 5; do
   echo "*** Trial ${trial} ***"
-  ./naive
+  ./stride
 done
 
 echo ""
